e3861a7dad16ece36354af388fc4620b1e96d58b
jalensailin <jalen.michalslevy@gmail.com>
2025-05-26 20:36:30 -0400

    read dirpath and output most recent file;

---
+++ podcast-automation.py
@@ -0,0 +1,8 @@
+from glob import glob
+import os
+
+dirpath = "/media/jalensailin/Store - HDD (1 TB)/OBS Recordings"
+list_of_files = glob(f"{dirpath}/*.mkv")
+
+currentFile = max(list_of_files, key=os.path.getctime)
+print(currentFile)
