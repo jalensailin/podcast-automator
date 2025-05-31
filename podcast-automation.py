e472113c4dc1b35dd83f0877ecfd7e909087e8f5
jalensailin <jalen.michalslevy@gmail.com>
2025-05-30 23:08:50 -0400

    implement ffmpeg mkv -> mp3 subprocess; changed some variable names as well

--- podcast-automation.py
+++ podcast-automation.py
@@ -1,15 +1,33 @@
 from glob import glob
 import json
 import os
+import subprocess
 
-# Load input directory path
+# Get paths from secrets file.
 with open("./secrets.json") as file:
     secrets = json.load(file)
-    dirpath = secrets["directory_path"]
+    input_path = secrets["input_path"]
+    output_path = secrets["output_path"]
 
-list_of_files = glob(f"{dirpath}/*.mkv")
+# Get most recent file in input directory.
+list_of_files = glob(f"{input_path}/*.mkv")
+current_file = max(list_of_files, key=os.path.getctime)
 
-currentFile = max(list_of_files, key=os.path.getctime)
-print(currentFile)
+# Call ffmpeg on input file.
+print(f"Converting file '{os.path.basename(current_file)}' to .mp3")
+sp = subprocess.run(
+    [
+        "ffmpeg",
+        "-i",
+        current_file,
+        "-vn",
+        "-c:a",
+        "libmp3lame",
+        "-b:a",
+        "64k",
+        f"{output_path}/test.mp3",
+    ],
+    capture_output=True,
+)
 
-# ffmpeg -output "/some-dir" -input "/some-dir/*.mkv"
+# print(sp.stdout) # TODO: This line doesn't quite work yet
